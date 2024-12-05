72afa6d4d6cde10096aa52939aa7a3b5e55ce642
Stmg2025 <cthompson@maigas.cl>
2024-12-05 05:34:51 -0300

    vuelta

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-import axios from "axios";
+const config = {
+    backendUrl: "https://backrepuestock.vercel.app",
+};
 
-const apiClient = axios.create({
-    baseURL: "https://backrepuestock.vercel.app", // Cambia esto por la URL correcta del backend
-});
-
-export default apiClient;
+export default config;
